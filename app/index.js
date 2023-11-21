39ff6bdf87c1dc9272ef1a1df2d33c989eb1d3fe
MANUEL RIOS <m.rios56@hotmail.com>
2023-11-21 18:47:49 -0400

    first commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Hello World from RN</Text>
+        </View>
+    ) 
+
+}
+
+export default Home 
